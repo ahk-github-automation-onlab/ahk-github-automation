c9fb9006b0e626eed2255782eb99fb478d22ea22
Barnabás Sörös <soros14@gmail.com>
2023-03-28 11:20:57 +0200

    formatting fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,16 +33,16 @@ resource "azurerm_service_plan" "service_plan" {
 }
 
 resource "azurerm_windows_function_app" "function_app" {
-    name                      = "ahk-github-function-app"
-    location                  = azurerm_resource_group.resource_group.location
-    resource_group_name       = azurerm_resource_group.resource_group.name
-    
-    
-    storage_account_name       = azurerm_storage_account.storage.name
-    storage_account_access_key = azurerm_storage_account.storage.primary_access_key
-    service_plan_id            = azurerm_service_plan.service_plan.id
+  name                = "ahk-github-function-app"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+  service_plan_id            = azurerm_service_plan.service_plan.id
 
 
 
-    site_config {}
+  site_config {}
 }
